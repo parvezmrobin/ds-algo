8bdee5fed7ecd7ff04ebadb7d389a729830479b4
parvezmrobin@gmail.com <qwer56uiop>
2018-12-31 02:15:49 +0600

    cost type Number to double, index validation

--- src/com/parvezmrobin/ds/Graph.java
+++ src/com/parvezmrobin/ds/Graph.java
@@ -6,7 +6,7 @@ import com.parvezmrobin.ds.contracts.GraphInterface;
 
 public class Graph implements GraphInterface {
 
-    private Number[][] graph;
+    private double[][] graph;
     private int numberOfNodes;
     private boolean isBidirectional;
 
@@ -15,7 +15,7 @@ public class Graph implements GraphInterface {
     }
 
     public Graph(int numberOfNodes, boolean isBidirectional) {
-        this.graph = new Number[numberOfNodes][numberOfNodes];
+        this.graph = new double[numberOfNodes][numberOfNodes];
         for (int i = 0; i < numberOfNodes; i++) {
             for (int j = 0; j < numberOfNodes; j++) {
                 this.graph[i][j] = Double.POSITIVE_INFINITY;
@@ -26,20 +26,44 @@ public class Graph implements GraphInterface {
         this.isBidirectional = isBidirectional;
     }
 
+    public int getNumberOfNodes() {
+        return numberOfNodes;
+    }
+
     @Override
-    public void addPath(int from, int to, Number cost) {
+    public void addPath(int from, int to, double cost) {
+        validateIndices(from, to);
         this.graph[from][to] = cost;
         if (this.isBidirectional) {
             this.graph[to][from] = cost;
         }
     }
 
+    private void validateIndices(int from, int to) {
+        validateFromIndex(from);
+        validateToIndex(to);
+    }
+
+    private void validateToIndex(int to) {
+        if (to >= this.numberOfNodes) {
+            throw new IndexOutOfBoundsException("to " + to + " index out of bound.");
+        }
+    }
+
+    private void validateFromIndex(int from) {
+        if (from >= this.numberOfNodes) {
+            throw new IndexOutOfBoundsException("from " + from + " index out of bound.");
+        }
+    }
+
     public void addPath(int from, int to) {
+        this.validateIndices(from, to);
         this.addPath(from, to, 1);
     }
 
     @Override
     public void removePath(int from, int to) {
+        this.validateIndices(from, to);
         this.graph[from][to] = Double.POSITIVE_INFINITY;
         if (this.isBidirectional) {
             this.graph[to][from] = Double.POSITIVE_INFINITY;
@@ -47,16 +71,17 @@ public class Graph implements GraphInterface {
     }
 
     @Override
-    public Number[] getChildren(int parent) {
+    public int[] getChildren(int parent) {
+        this.validateFromIndex(parent);
         int childCount = 0;
         for (int i = 0; i < this.numberOfNodes; i++) {
-            if (this.graph[parent][i] instanceof Double && (double) this.graph[parent][i] != Double.POSITIVE_INFINITY) {
+            if (this.graph[parent][i] != Double.POSITIVE_INFINITY) {
                 childCount++;
             }
         }
-        Number[] children = new Number[childCount];
+        int[] children = new int[childCount];
         for (int i = 0, j = 0; j < childCount; i++) {
-            if (this.graph[parent][i] instanceof Double && (double) this.graph[parent][i] != Double.POSITIVE_INFINITY) {
+            if (this.graph[parent][i] != Double.POSITIVE_INFINITY) {
                 children[j++] = i;
             }
         }
@@ -65,7 +90,8 @@ public class Graph implements GraphInterface {
     }
 
     @Override
-    public Number getCost(int from, int to) {
+    public double getCost(int from, int to) {
+        this.validateIndices(from, to);
         return this.graph[from][to];
     }
 }
--- src/com/parvezmrobin/ds/contracts/GraphInterface.java
+++ src/com/parvezmrobin/ds/contracts/GraphInterface.java
@@ -1,12 +1,13 @@
 package com.parvezmrobin.ds.contracts;
 
 public interface GraphInterface {
+    public int getNumberOfNodes();
 
-    public void addPath(int from, int to, Number cost);
+    public void addPath(int from, int to, double cost);
 
     public void removePath(int from, int to);
     
-    public Number[] getChildren(int parent);
+    public int[] getChildren(int parent);
 
-    public Number getCost(int from, int to);
+    public double getCost(int from, int to);
 }
