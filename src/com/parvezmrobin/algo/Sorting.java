008e53cff518a4d5f467b1e387fded094092809a
parvezmrobin@gmail.com <qwer56uiop>
2018-12-31 06:17:27 +0600

    + Sorting{bubbleSort(), selectionSort()}

--- src/com/parvezmrobin/algo/Sorting.java
+++ src/com/parvezmrobin/algo/Sorting.java
@@ -2,6 +2,7 @@ package com.parvezmrobin.algo;
 
 import com.parvezmrobin.ds.Heap;
 import com.parvezmrobin.ds.MaxHeap;
+import org.jetbrains.annotations.NotNull;
 
 public class Sorting {
     public static double[] heapSort(double[] array) {
@@ -13,19 +14,47 @@ public class Sorting {
         return sortedArray;
     }
 
-    public static int[] heapSort(int[] array){
-        double[] doubleArray = new double[array.length];
-        for (int i = 0; i < array.length; i++) {
-            doubleArray[i] = (double) array[i];
+    public static double[] bubbleSort(double[] array){
+        double[] copy = makeCopy(array);
+
+        for (int i = 1; i < copy.length; i++) {
+            double temp = copy[i];
+            int j;
+            for (j = i; j > 0; j--) {
+                if (copy[j-1] > temp) {
+                    copy[j] = copy[j-1];
+                } else {
+                    break;
+                }
+            }
+            copy[j] = temp;
         }
 
-        double[] sortedArray = heapSort(doubleArray);
+        return copy;
+    }
 
-        int[] sortedIntArray = new int[array.length];
-        for (int i = 0; i < array.length; i++) {
-            sortedIntArray[i] = (int) sortedArray[i];
+    public static double[] selectionSort(double[] array){
+        double[] copy = makeCopy(array);
+
+        for (int i = 0; i < copy.length - 1; i++) {
+            int min_index = i;
+            for (int j = i; j < copy.length; j++) {
+                if (copy[j] < copy[min_index]){
+                    min_index = j;
+                }
+            }
+
+            double temp = copy[i];
+            copy[i] = copy[min_index];
+            copy[min_index] = temp;
         }
 
-        return sortedIntArray;
+        return copy;
+    }
+
+    private static double[] makeCopy(@NotNull double[] array) {
+        double[] copy = new double[array.length];
+        System.arraycopy(array, 0, copy, 0, array.length);
+        return copy;
     }
 }
--- test/com/parvezmrobin/algo/SortingTest.java
+++ test/com/parvezmrobin/algo/SortingTest.java
@@ -19,14 +19,26 @@ class SortingTest {
     }
 
     @Test
-    void heapSortInt() {
-        int[] array = {5, 4, 3, 2, 1};
-        assertArrayEquals(new int[]{5, 4, 3, 2, 1}, Sorting.heapSort(array));
-        array = new int[]{3, 4, 2, 1, 5};
-        assertArrayEquals(new int[]{5, 4, 3, 2, 1}, Sorting.heapSort(array));
-        array = new int[]{5, 2, 1, 2, 2};
-        assertArrayEquals(new int[]{5, 2, 2, 2, 1}, Sorting.heapSort(array));
-        array = new int[]{1, 2, 3, 4, 5};
-        assertArrayEquals(new int[]{5, 4, 3, 2, 1}, Sorting.heapSort(array));
+    void bubbleSort() {
+        double[] array = {5, 4, 3, 2, 1};
+        assertArrayEquals(new double[]{1, 2, 3, 4, 5}, Sorting.bubbleSort(array));
+        array = new double[]{3, 4, 2, 1, 5};
+        assertArrayEquals(new double[]{1, 2, 3, 4, 5}, Sorting.bubbleSort(array));
+        array = new double[]{5, 2, 1, 2, 2};
+        assertArrayEquals(new double[]{1, 2, 2, 2, 5}, Sorting.bubbleSort(array));
+        array = new double[]{1, 2, 3, 4, 5};
+        assertArrayEquals(new double[]{1, 2, 3, 4, 5}, Sorting.bubbleSort(array));
+    }
+
+    @Test
+    void selectionSort() {
+        double[] array = {5, 4, 3, 2, 1};
+        assertArrayEquals(new double[]{1, 2, 3, 4, 5}, Sorting.selectionSort(array));
+        array = new double[]{3, 4, 2, 1, 5};
+        assertArrayEquals(new double[]{1, 2, 3, 4, 5}, Sorting.selectionSort(array));
+        array = new double[]{5, 2, 1, 2, 2};
+        assertArrayEquals(new double[]{1, 2, 2, 2, 5}, Sorting.selectionSort(array));
+        array = new double[]{1, 2, 3, 4, 5};
+        assertArrayEquals(new double[]{1, 2, 3, 4, 5}, Sorting.selectionSort(array));
     }
 }
